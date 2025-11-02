40340d3e783442938f1ff0101899d43f4eb94881
Aquib-Alvi-au45 <aquibalvi831@gmail.com>
2025-11-02 18:31:06 +0530

    solved areThereDuplicates with multiple pointers

--- areThereDuplicates.jsx
+++ areThereDuplicates.jsx
@@ -10,7 +10,19 @@ function areThereDuplicatesFrequency(...values) {
   return false;
 }
 
-console.log(areThereDuplicates(1, 2, 3));
+// console.log(areThereDuplicatesFrequency(1, 2, 3));
 
+function areThereDuplicatesPointer(...values) {
+  let sortedValues = values.sort();
+  let j = 1;
+  for (let i = 0; i < sortedValues.length-1; i++) {
+    if(sortedValues[i]===sortedValues[j]){
+      return true
+    }else{
+      j++
+    }
+  }
+  return false
+}
 
-
+console.log(areThereDuplicatesPointer(3, 1,3, 2));
