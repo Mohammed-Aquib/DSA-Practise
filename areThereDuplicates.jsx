99cdb66ab76420dbc04ced8f7bf325e708d092db
Aquib-Alvi-au45 <aquibalvi831@gmail.com>
2025-11-02 17:26:57 +0530

    areThereDuplicates

--- areThereDuplicates.jsx
+++ areThereDuplicates.jsx
@@ -13,12 +13,4 @@ function areThereDuplicatesFrequency(...values) {
 console.log(areThereDuplicates(1, 2, 3));
 
 
-function areThereDuplicatesPointer(...values) {
- for (let i =0;i<values.length;i++){
-    
- }
-}
-
-console.log(areThereDuplicates(1, 2, 3));
-
 
